b30458795a2efaebdffda2c8eb0dae6c17006b7f
Daniel Andulahad <daniel00.abdulahad@gmail.com>
2020-06-17 18:45:32 +0200

    added MonoGame project

---
+++ TowerDefence/SpriteManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TowerDefence
+{
+    public static class SpriteManager
+    {
+        private static Dictionary<string, Texture2D> sprites = new Dictionary<string, Texture2D>();
+
+        public static void Add(string name, Texture2D sprite)
+        {
+            if (!sprites.ContainsKey(name)) sprites.Add(name, sprite);
+            else sprites[name] = sprite;
+        }
+
+        public static Texture2D Get(string name)
+        {
+            if (sprites.TryGetValue(name, out Texture2D sprite)) return sprite;
+            return null;
+        }
+    }
+}
---
+++ TowerDefence/Towers/TestTower.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TowerDefence.Towers
+{
+    public class TestTower : Tower
+    {
+        public TestTower()
+            : base(10f, new Rectangle(), SpriteManager.Get("test"))
+        {
+        }
+
+        public override void Update(float deltaTime)
+        {
+        }
+
+        public override void Draw(float deltaTime)
+        {
+        }
+    }
+}
